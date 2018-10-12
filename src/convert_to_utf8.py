016196772ad86d2c658ec5bc2cb87066feb22065
Matt Burns <burns963@yahoo.com>
2018-10-12 09:34:50 -0500

    Make another dummy Python file.

--- src/convert_to_utf8.py
+++ src/convert_to_utf8.py
@@ -1,5 +1,24 @@
-raw = open('avengers.csv', 'rb').read()
-decoded = raw.decode ('ISO-8859-1')
-encoded = decoded.encode ('utf-8')
-with open ('avengers_utf8.csv', 'w') as file:
-    file.write(decoded)
+import sys
+ 
+def argumentExists(index): #function to try get arguments that will not crash program if argument out of bounds
+    try:
+        sys.argv[index]
+    except IndexError:
+        return ''
+    else:
+        return sys.argv[index]
+ 
+def conversion(input, output): #function to write out file.
+    avengers = open(input,'rb')
+    avengersdecoded = avengers.read()
+    avengersdecoded = avengersdecoded.decode('ISO-8859-1')
+    avengers.close()
+    writeAvengers = open(output, 'w')
+    writeAvengers.write(avengersdecoded)
+    writeAvengers.close()
+ 
+if __name__ == "__main__":
+    targetFile = argumentExists(1)
+    resultFile = argumentExists(2)
+    if targetFile and resultFile: #both arguments have value
+        conversion(targetFile, resultFile)
